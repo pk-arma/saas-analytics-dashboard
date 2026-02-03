6b29d36a68a2ab84eb7a4f663c6791dde8e56512
rattan <pankaj.vishwakarma@rattanindia.com>
2026-02-03 17:39:17 +0530

    clear up the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,8 @@
-'use client';
-import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [items, setItems] = useState([]);
-  const [form, setForm] = useState({ name: '', description: '' });
-
-
-  // const fetchItems = async () => {
-  //   const res = await fetch('/api/items');
-  //   const data = await res.json();
-  //   setItems(data.data);
-  // };
-
-  // useEffect(() => {
-  //   eslint-disable-next-line react-hooks/set-state-in-effect
-  //   fetchItems();
-  // }, []);
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await fetch('/api/items', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
-      // fetchItems();
-      setForm({ name: '', description: '' });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
-    <div>
-      <h1>Items</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="name"
-          placeholder="Item name"
-          value={form.name}
-          onChange={handleChange}
-        />
-        <input
-          name="description"
-          placeholder="Item description"
-          value={form.description}
-          onChange={handleChange}
-        />
-        <button type="submit">Add Item</button>
-      </form>
-
-      <ul>
-        {items.map((item) => (
-          <li key={item._id}>
-            {item?.name} - {item?.description}
-          </li>
-        ))}
-      </ul>
+    <div className="flex min-h-screen items-center justify-center  font-sans">
+      This is Saas analytics dashboard
     </div>
   );
 }
-// export default function Home() {
-//   return (
-//     <div className="flex min-h-screen items-center justify-center  font-sans">
-//       This is Saas analytics dashboard
-//     </div>
-//   );
-// }
